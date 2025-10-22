65efabb9df690faf36bac1f8346fc0e89fa6c0f3
Motiejus Šveikauskas <118689022+Motiejukaas@users.noreply.github.com>
2025-10-22 11:42:47 +0300

    Initial Commit

---
+++ src/main/java/com/motiejus/eventsync/EventsyncApplication.java
@@ -0,0 +1,13 @@
+package com.motiejus.eventsync;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventsyncApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(EventsyncApplication.class, args);
+    }
+
+}
